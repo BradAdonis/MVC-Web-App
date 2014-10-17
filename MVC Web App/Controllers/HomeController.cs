49e28a31690ea410a0bb9e928b43a60e4a971cab
BradAdonis <bradley.cunningham@gmail.com>
2014-10-17 14:28:51 +0200

    2nd

--- MVC Web App/Controllers/HomeController.cs
+++ MVC Web App/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using MVC_Web_App.Models;
+using Ninject;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,12 @@ namespace MVC_Web_App.Controllers
 {
     public class HomeController : Controller
     {
+        Product[] products = {new Product{Name = "Kayak", Price=275M},
+                                  new Product{Name = "Life jacket",Price=48.96M},
+                                  new Product{Name = "Soccerball", Price=19.50M},
+                                  new Product{Name = "Corner", Price=34.95M}
+                              };
+
         Product myProduct = new Product { 
             ProductID = 1,
             Name = "Kayak",
@@ -41,13 +48,29 @@ namespace MVC_Web_App.Controllers
 
         public ActionResult DemoArray()
         {
-            Product[] array = {new Product{Name = "Kayak", Price=275M},
-                                  new Product{Name = "Life jacket",Price=48.96M},
-                                  new Product{Name = "Soccerball", Price=19.50M},
-                                  new Product{Name = "Corner", Price=34.95M}
-                              };
+            return View(products);
+        }
 
-            return View(array);
+        //old way without dependency injection
+        public ActionResult Calculations()
+        {
+            IValueCalculator calc = new LinqValueCalculator();
+            ShoppingCart cart = new ShoppingCart(calc) { Products = products};
+            decimal totalValue = cart.CalculateProductTotal();
+            return View(totalValue);
+        }
+
+        //new method using dependancy injection
+        public ActionResult CalculationsDI()
+        {
+            IKernel ninject = new StandardKernel();
+            ninject.Bind<IValueCalculator>().To<LinqValueCalculator>();
+
+            IValueCalculator calc = ninject.Get<IValueCalculator>();
+            ShoppingCart cart = new ShoppingCart(calc) { Products = products };
+            decimal totalValue = cart.CalculateProductTotal();
+
+            return View(totalValue);
         }
     }
 }
---
+++ MVC Web App/Models/IValueCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Web_App.Models
+{
+    public interface IValueCalculator
+    {
+        decimal ValueProducts(IEnumerable<Product> products);
+    }
+}
